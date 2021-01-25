84bda31801dbeb6a10070b87ce03a5a531e83812
SirSydom <com@sirsydom.de>
2021-01-25 12:01:01 +0100

    V0.04 improved E3, removed A2

--- 1658_Differenzwert (remanent)/19001658_lbs.php
+++ 1658_Differenzwert (remanent)/19001658_lbs.php
@@ -1,16 +1,14 @@
 ###[DEF]###
-[name		=Differenzwert (remanent) LBS 1658 V0.03	]
+[name		=Differenzwert (remanent) LBS 1658 V0.04	]
 
 [e#1 TRIGGER=Trigger/Stop #init=INIT		]
 [e#2		=Messwert #init=INIT			]
 [e#3		=Messwertkorrektur #init=INIT			]
 
 [a#1		=&Delta;Wert					]
-[a#2		=&Delta;Zeit					]
 [a#3		=Archiv							]
 
 [v#1	remanent	=						]
-[v#2	remanent	=						]
 ###[/DEF]###
 
 
@@ -18,29 +16,27 @@
 Dieser Baustein berechnet die Differenz von nacheinander eintreffenden Werten, z.B. um die Differenz eines Zählerstandes in Abhängigkeit eines Ereignisses zu ermitteln.
 
 Ein Telegramm &ne;0 an E1 startet die Messung, bzw. startet die Messung neu:
-A1 und A2 werden auf 0 gesetzt. Der Wert an E2 wird als Referenzwert intern gespeichert (inklusive Zeitstempel).
-A3 wird auf die gesamte Laufzeit der letzten Messung gesetzt (wenn es eine gab).
-Jedes eintreffende Telegramm an E2 während einer Messung (E1&ne;0) führt zu einer Aktualisierung von A1 (Wertdifferenz) und A2 (Zeitdifferenz).
+A1 wird auf 0 gesetzt. Der Wert an E2 wird als Referenzwert intern gespeichert.
+A3 wird auf den Messwert der letzten Differenzmessung gesetzt (nur beim Neustart / Stoppen).
+Jedes eintreffende Telegramm an E2 während einer Messung (E1&ne;0) führt zu einer Aktualisierung von A1 (Wertdifferenz).
 
 Ein Telegramm =0 an E1 beendet die Messung:
 A1 bleibt unverändert, da eine Wertänderung an E2 während der Messung A1 bereits entsprechend gesetzt hat.
-A2 wird auf die gesamte Laufzeit der Messung gesetzt.
-A3 wird auf die gesamte Laufzeit der Messung gesetzt.
-A1 und A2 verhalten sich also wie SBC-Ausgänge (Send-By-Change).
+A3 wird auf den Messwert der letzten Differenzmessung gesetzt.
+
 
 Hinweise:
 Telegramme =0 an E1 werden ignoriert, wenn zuvor keine Messung mit einem Telegramm &ne;0 an E1 gestartet wurde.
-Wird E2 nicht belegt bzw. nicht verändert, verhält sich der Baustein wie eine einfache Stoppuhr (A2 wird auf die Zeitdifferenz zwischen E1&ne;0 und E1=0 gesetzt).
+
  
 Achtung:
-Eine Aktualisierung von A1 und A2 erfolgt nur beim Starten und Stoppen einer Messung und bei eintreffenden Telegrammen an E2 während einer Messung. Es erfolgt <i>keine</i> zyklische Änderung von A1 und A2!
+Eine Aktualisierung von A1 erfolgt nur beim Starten und Stoppen einer Messung und bei eintreffenden Telegrammen an E2 während einer Messung. Es erfolgt <i>keine</i> zyklische Änderung von A1!
  
  
 E1: Starten (&ne;0) bzw. Stoppen (=0) einer Messung (Achtung: um unerwünschte Effekte bei der Initialisierung zu unterbinden, sollte E1 mit 'INIT' initialisiert werden(default))
 E2: Messwert (nummerisch), dessen Differenz berechnet werden soll (z.B. ein Zählerstand) (Achtung: um unerwünschte Effekte bei der Initialisierung zu unterbinden, sollte E2 mit 'INIT' initialisiert werden(default))
-E3: Ein Telegram setzt den Startwert von E2 auf den Wert von E3. Muss danach wieder auf 'INIT' zurückgesetzt werden.
+E3: Ein Telegram setzt den Startwert von E2 auf den Wert von E3. 
 A1: Messwert-Differenz (nummerisch): wird beim Start auf 0 gesetzt, dann bei jedem eintreffenden Telegramm an E2 auf die Wertdifferenz, beim Beenden der Messung erfolgt keine Änderung
-A2: Zeitdifferenz (Sekunden, FLOAT): wird beim Start auf 0 gesetzt, dann bei jedem eintreffenden Telegramm an E2 auf die Zeitdifferenz, beim Beenden der Messung auf die gesamte Zeitdifferenz der Messung
 A3: Letzter Differenzwert beim Stoppen oder Neustarten einer Messung (z.B. zur Archivierung)
 ###[/HELP]###
 
@@ -52,25 +48,23 @@ function LB_LBSID($id)
 	if (($E=logic_getInputs($id)) && ($V=logic_getVars($id))) {
 		
 		//Messwert korrigieren 
-		if (!isEmpty($V[2]) && $E[3]['refresh']==1 && $E[3]['value']!=='INIT' ) {
+		if ($E[3]['refresh']==1 && $E[3]['value']!=='INIT' ) {
 			logic_setVar($id,1,$E[3]['value']);
 		}
 		
 		//Differenzen ausgeben
-		if (!isEmpty($V[2]) && $E[2]['refresh']==1 && $E[2]['value']!=='INIT' ) {
+		if (!isEmpty($V[1]) && $E[2]['refresh']==1 && $E[2]['value']!=='INIT' ) {
 			logic_setOutput($id,1,$E[2]['value']-$V[1]);
-			logic_setOutput($id,2,getMicrotime()-$V[2]);
 		}
 	
 		if ($E[1]['refresh']==1)
 		{	// neues trigger telegram
 			if ($E[1]['value']==0 && $E[1]['value']!=='INIT')
 			{	//Stop
-				if (!isEmpty($V[2]))
+				if (!isEmpty($V[1]))
 				{
 					logic_setOutput($id,3,$E[2]['value']-$V[1]);
-					logic_setVar($id,2,null);
-					logic_setOutput($id,2,getMicrotime()-$V[2]);
+					logic_setVar($id,1,null);
 				}
 				else
 				{
@@ -81,17 +75,13 @@ function LB_LBSID($id)
 			{
 				//Start
 				// variablen schreiben, ausgänge neu setzen
-				if(!isEmpty($V[2]))
+				if(!isEmpty($V[1])) // Neustart
 					logic_setOutput($id,3,$E[2]['value']-$V[1]);
 				logic_setVar($id,1,$E[2]['value']);
-				logic_setVar($id,2,getMicrotime());
 				logic_setOutput($id,1,0);
-				logic_setOutput($id,2,0);
 			}
 		}
-
 	}
-
 }
 ?>
 ###[/LBS]###
